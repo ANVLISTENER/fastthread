1ccd074d0599c315e515118bd1842fd79f1d4f95
ANVLISTNER <libin.ravi0601@gmail.com>
2026-05-13 02:12:34 +0000

    src.

---
+++ examples/thread_perf.py
@@ -0,0 +1,24 @@
+from threading import Thread
+import time
+
+def on_complete(result):
+    print(f"Hello, {result}")
+    pass
+
+# 1. Start the Timer
+start_time = time.perf_counter()
+
+# 2. Run the C++ Worker
+# We use 10,000,000 as before
+worker = Thread(target=on_complete, args=(10000,))
+
+# 3. Start Immediately
+worker.start()
+
+# 3. Join immediately
+# This waits ONLY for the C++ code to finish
+worker.join()
+
+end_time = time.perf_counter()
+
+print(f"Execution Time: {end_time - start_time:.6f} seconds")
